74b282eb4f09b336781f05fbabc1d262a24c2ed4
miracule <32400382+miracule@users.noreply.github.com>
2022-10-30 12:26:22 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/tecno/TECNO-BC3
+DEVICE_PATH := device/tecno/BC3
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
