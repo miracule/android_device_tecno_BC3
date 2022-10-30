431dd27657616a653d314f0b8469207297ab8205
miracule <32400382+miracule@users.noreply.github.com>
2022-10-30 12:40:57 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_TECNO-BC3-user
-add_lunch_combo omni_TECNO-BC3-userdebug
-add_lunch_combo omni_TECNO-BC3-eng
+add_lunch_combo omni_BC3-user
+add_lunch_combo omni_BC3-userdebug
+add_lunch_combo omni_BC3-eng
